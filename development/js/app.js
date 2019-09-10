b333bd8a856170c5e906a6e2003f02c4b98fb25d
Weronika Chmiel <weronika.z.chmiel@gmail.com>
2019-09-10 22:02:47 +0200

    poprawione zadanie z local storage - dziala

--- development/js/app.js
+++ development/js/app.js
@@ -19,54 +19,60 @@ for (let i = 0; i < arrayAllLi.length; i++) {
 
 
 
-// ======================== wysyłanie formularza cz1 =========================
 
-let input = document.querySelector(".main-app-input");
-let btn = document.querySelector(".main-app-button");
+// ======== sprawdzenie czy imię jest w local storage, jeśli tak to podmiana imienia ========
 
-btn.addEventListener("click", function () {
-    if (input.value.length < 3 || input.value.length > 15) {
-        input.value = "";
-        return alert("Imię musi posiadać od 3 do 15 znaków.")
-    }
+
+let name = document.querySelector(".main-app-input-name");
+name.innerText = localStorage.getItem("savedName");
 
 
 
+if (!(localStorage.getItem("savedName"))) {
 
-
-    // ======================== local storage =========================
-
-    let userName = input.value;
-    localStorage.setItem("savedName", userName);
-
-    console.log(localStorage.getItem("savedName"));
-
-
-
-
-
-
-// ======================== wysyłanie formularza cz2 =========================
-
-    let name = document.querySelector(".main-app-input-name");
-    name.innerText = localStorage.getItem("savedName");
-
-    let welcomeScreen = document.querySelector(".main-app-welcome-screen");
-    welcomeScreen.style.display = "none";
-
-
-
-// ======================== pokazanie pulpitu z funkcjami =========================
+    // ====== jeśli nie, to formularz do wpisania imienia cz1 ======
 
     let elementWithButtons = document.querySelector(".main-app-buttons");
-    let buttons = elementWithButtons.querySelectorAll("button");
-    console.log(buttons);
+    elementWithButtons.style.display = "none";
 
 
-    buttons.forEach(function (el) {
-        el.classList.add("visible");
+    let mainAppDashboard = document.querySelector(".main-app-welcome-screen");
+    mainAppDashboard.style.display = "flex";
+
+
+
+    let btn = document.querySelector(".main-app-button");
+
+    btn.addEventListener("click", function () {
+        let input = document.querySelector(".main-app-input");
+        if (input.value.length < 3 || input.value.length > 15) {
+            input.value = "";
+            return alert("Imię musi posiadać od 3 do 15 znaków.")
+        }
+
+
+        // ======================== local storage =========================
+
+        let userName = input.value;
+        localStorage.setItem("savedName", userName);
+        console.log(localStorage.getItem("savedName"));
+
+
+
+// =======  podmienienie imienia na górze z local storage i ukrycie formularza =======
+
+        name.innerText = localStorage.getItem("savedName");
+        mainAppDashboard.parentElement.removeChild(mainAppDashboard);
+
+        // mainAppDashboard.style.display = "none";
+
+
+// ============== pokazanie pulpitu z funkcjami po wysłaniu formularza ==============
+
+        elementWithButtons.style.display = "flex";
+
     });
-});
+}
 
 
 
